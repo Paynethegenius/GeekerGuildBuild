a47e61955c5ee6c472369bdaa2a680cd00cf7a21
Paynethegenius <62819789+Paynethegenius@users.noreply.github.com>
2022-10-11 01:58:45 +0100

    Play interface added

--- src/Characters/Soundo.js
+++ src/Characters/Soundo.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./soundo.css";
 import soundoArt1 from "../Images/Saly-24.png";
 import soundoArt2 from "../Images/Saly-37.png";
 import twitterLogo from "../Images/tweeter.png";
 import instagramLogo from "../Images/instagram.png";
 import linkedinLogo from "../Images/Linked.png";
-
 import Medal from "../Images/Medal.png";
-import BCSLogo from "../Images/BCS_logo.png";
-import UnibenLogo from "../Images/Uniben-Logo.png";
 
-import Avatar1 from "../Images/Avatar1.png";
-import Avatar2 from "../Images/Avatar2.png";
-import Avatar3 from "../Images/Avatar3.png";
+import pause from "../Svg/pause.svg";
+import play from "../Svg/play.svg";
+import previous from "../Svg/skip_previous.svg";
+import next from "../Svg/skip_next.svg";
+import { soundObject } from "../Data/audio.js";
+
 function Soundo() {
+  const [currentSound, setCurrentSound] = useState(soundObject.radios);
+  const [mode, setMode] = useState({ jingle : true, radio : false, music : false, tpye : "radio" });
+
+  const modeSet = {
+    
+  }
+
   return (
     <div className="soundo__page">
       <div className="soundo__leftpanel">
@@ -53,16 +60,21 @@ function Soundo() {
                         As a Creative Sound Engineer
                       </div>
                       <div className=" soundo__points soundo__two">
-                        <p>From a script, to a well articulated delivery that will
-                        sell your product, from a simple melody line
-                        to a song that expresses a mindset or touts a product.
-</p>
+                        <p>
+                          From a script, to a well articulated delivery that
+                          will sell your product, from a simple melody line to a
+                          song that expresses a mindset or touts a product.
+                        </p>
                         <br></br>
-                        <p> He is capably endowed with the ability to bring visions
-                        to life, imagined by greats, rendered by
-                        awesomes, delivered with high quality.</p>
-                        
-                        <br></br><p>Hear a few of my works.</p>
+                        <p>
+                          {" "}
+                          He is capably endowed with the ability to bring
+                          visions to life, imagined by greats, rendered by
+                          awesomes, delivered with high quality.
+                        </p>
+
+                        <br></br>
+                        <p>Hear a few of my works.</p>
                       </div>{" "}
                     </div>
                   </div>
@@ -99,7 +111,45 @@ function Soundo() {
           <section className="soundo__rightpanel__body__mid">
             <div className="soundo__rightpanel__body__mid__container">
               <div className="soundo__rightpanel__body__mid__container__content">
-                
+                <div className="soundo__musicplayer">
+                  <div className="soundo__musicplayer__container">
+                    <div className="soundo__musicplayer__container__top">
+                      <p onClick={()=>setCurrentSound(soundObject.jingles)}>JINGLES</p> 
+                      <p onClick={()=>setCurrentSound(soundObject.radios)}> RADIO </p> 
+                      <p onClick={()=>setCurrentSound(soundObject.sounds)}>MUSIC</p>
+                    </div>
+                    <div className="soundo__musicplayer__container__mid">
+                      <div className="soundo__musicplayer__container__mid__buttons">
+                        <img src={previous} alt="previoua" />
+                        <img src={play} alt="play" />
+                        <img src={next} alt="next" />
+                      </div>
+                    </div>
+                    <div className="soundo__musicplayer__container__bottom">
+                      GOtv, Ember To Remember
+                    </div>
+                  </div>
+                </div>
+                <div className="soundo__music__list">
+                  <ul className="soundo__music__list_items">
+                   {currentSound.map((item)=>{
+                    return( <li className="soundo__music__list_item">
+                    <div className="soundo__music__list_item__displayPic">
+                      <img src={Medal} alt="GOTV" />
+                    </div>
+                    <div className="soundo__music__list_item__soundDetails">
+                      <p className="soundlabel">
+                        {item.name}
+                      </p>
+                      <br></br>
+                       <p className="soundtype">Radio Commercial</p> 
+                    </div>
+                  </li>)
+                   })}
+                   
+                  
+                  </ul>
+                </div>
               </div>
             </div>
           </section>
---
+++ src/Data/audio.js
@@ -0,0 +1,36 @@
+import soundA from "../Audio/Soundo/Sounds/1.121 NITRO FULL MAST.mp3";
+import soundB from "../Audio/Soundo/Sounds/2.Arla Dano English Orchestral.mp3";
+import soundC from "../Audio/Soundo/Sounds/3.Arla Dano Igbo HighLife for radio.mp3";
+import soundD from "../Audio/Soundo/Sounds/4.Demo for CloseUp.mp3";
+import soundE from "../Audio/Soundo/Sounds/5.GOTv Showtime Sinature tune.mp3";
+
+import radioA from "../Audio/Soundo/Radios/1.Arla Dano Pidgin 45 SECS.mp3";
+import radioB from "../Audio/Soundo/Radios/2.Ember To Remember Final (1).mp3";
+import radioC from "../Audio/Soundo/Radios/3.GOTV Christmas Max Radio.mp3";
+import radioD from "../Audio/Soundo/Radios/4.GOtv Kids April May Radio.mp3";
+import radioE from "../Audio/Soundo/Radios/5.Gotv Nigerian Idol Radio.mp3";
+
+export const soundObject = {
+  jingles: [
+    { sound: soundA, name: "Nitro 121 Theme Song", Id: 1 },
+    { sound: soundB, name: "Arla Dano, Go for it English", Id: 2 },
+    { sound: soundC, name: "Arla Dano, Go for it, Igbo", Id: 3 },
+    { sound: soundD, name: "Close Up Demo", Id: 4 },
+    { sound: soundE, name: "GOtv Showtime Signature tune", Id: 5 },
+  ],
+
+  radios: [
+    { radio: radioA, name: "1.Arla Dano Pidgin 45 SECS", Id: 1 },
+    { radio: radioB, name: "2.Ember To Remember Final ", Id: 2 },
+    { radio: radioC, name: "3.GOTV Christmas Max Radio", Id: 3 },
+    { radio: radioD, name: "4.GOtv Kids April May Radio", Id: 4 },
+    { radio: radioE, name: "5.Gotv Nigerian Idol Radio", Id: 5 },
+  ],
+  sounds : [
+    { radio: radioA, name: "1.TimeFlies", Id: 1 },
+    { radio: radioB, name: "2.Alabama ", Id: 2 },
+    { radio: radioC, name: "3.GOTV Christmas Max Radio", Id: 3 },
+    { radio: radioD, name: "4.GOtv Kids April May Radio", Id: 4 },
+    { radio: radioE, name: "5.Gotv Nigerian Idol Radio", Id: 5 },
+  ],
+};
